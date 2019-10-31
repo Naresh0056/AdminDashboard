b04f5cc8370095517a8e2b01e4e647d07acf8c89
Naresh <naresh.mehta8800@gmail.com>
2019-10-31 17:08:21 +0530

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './shared-module/sidebar/sidebar.component';
-import { LoginComponent } from './auth/login/login.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { SidebarComponent } from "./shared-module/sidebar/sidebar.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,11 +14,8 @@ import { LoginComponent } from './auth/login/login.component';
     SidebarComponent,
     LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,6 +5,7 @@ import {
   ViewChild,
   AfterViewInit
 } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-login",
@@ -12,11 +13,40 @@ import {
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit, AfterViewInit {
-  @ViewChild("container", { static: false }) container: ElementRef;
+  // @ViewChild("container", { static: false }) container: ElementRef;
+  public loginForm: FormGroup;
+
   constructor() {}
 
-  ngOnInit() {}
-  ngAfterViewInit() {
-    console.log(this.container);
+  ngOnInit() {
+    this.loginFormValidation();
   }
+  private loginFormValidation() {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      // create new FormGroup
+
+      skills: new FormGroup({
+        skillName: new FormControl(),
+        expInyears: new FormControl(),
+        proficiency: new FormControl()
+      })
+    });
+  }
+  submitLoginForm(event): void {
+    console.log(this.loginForm.value);
+  }
+  public loadData(): void {
+    this.loginForm.patchValue({
+      username: "Naresh",
+      password: 1234657,
+      skills: {
+        skillName: "dddffd",
+        expInyears: 5,
+        proficiency: "Advanced"
+      }
+    });
+  }
+  ngAfterViewInit() {}
 }
