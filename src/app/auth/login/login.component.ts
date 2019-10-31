2073aaca57ea4de2af01c9d1ce12f97612c2a6fe
Naresh Nirala <nkmehta@craterzone.com>
2019-11-01 00:52:47 +0530

    mn kiya

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,18 +5,65 @@ import {
   ViewChild,
   AfterViewInit
 } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"]
 })
-export class LoginComponent implements OnInit, AfterViewInit {
-  @ViewChild("container", { static: false }) container: ElementRef;
-  constructor() {}
+export class LoginComponent implements OnInit {
+  public registrationForm: FormGroup;
+  public jsonData = {
+    email: "abc@gmail.com",
+    password: "ffgfgfhj",
+    addresses: {
+      address1: "beta 1",
+      address2: "beta 2",
+      city: "Greater Noida",
+      zip: "123456",
+      check: true
+    }
+  };
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.registrationFormValidation();
+  }
 
-  ngOnInit() {}
-  ngAfterViewInit() {
-    console.log(this.container);
+  private registrationFormValidation() {
+    // this.registrationForm = new FormGroup({
+    //   email: new FormControl(),
+    //   password: new FormControl(),
+    //   addresses: new FormGroup({
+    //     address1: new FormControl(),
+    //     address2: new FormControl(),
+    //     city: new FormControl(),
+    //     zip: new FormControl(),
+    //     check: new FormControl()
+    //   })
+    // });
+    // **********************Use FormBuilder
+    this.registrationForm = this.fb.group({
+      email: ["", Validators.required],
+      password: [""],
+      addresses: this.fb.group({
+        address1: [""],
+        address2: [""],
+        city: [""],
+        zip: [""],
+        check: [""]
+      })
+    });
+  }
+  public submitRegForm() {
+    console.log(this.registrationForm.value);
+  }
+  public loadData() {
+    this.registrationForm.setValue(this.jsonData);
   }
 }
